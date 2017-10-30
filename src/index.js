c834916010b291f11d1defc6dad7ee4db57ca0fc
CORP\desapm <manjnunath.desappa@emc.com>
2017-10-30 16:21:22 +0530

    es6 starter

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-import Hapi,{Server} from "hapi";
+const sayHello  = () => console.log("Hello");
 
-
-const server = new Server();
-
-server.connection({
-  port: 3000,
-  host: 'localhost'
-});
-
-
-server.start((err) => {
-  if(err) throw err;
-  else console.log(`Server Running at: ${server.info.uri}`);
-
-
-  let someAsyncStuff = async() => {
-    return 3;
-  };
-
-  let getAsyncStuff = async() => {
-    let x = await someAsyncStuff();
-    let y = await someAsyncStuff();
-
-    let z = await Promise.all([someAsyncStuff(), someAsyncStuff()]);
-
-    console.log(z);
-
-  };
-
-  getAsyncStuff();
-
-
-});
+sayHello();
